654054ad715a0a7565cc9f69ed1f6de1a01144ea
andi-s0106 <andress.933110@gmail.com>
2021-03-12 02:17:06 +0000

    updated binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -40,5 +40,6 @@ size_t binary_tree_depth(const binary_tree_t *tree);
 size_t binary_tree_size(const binary_tree_t *tree);
 size_t calc_size(const binary_tree_t *tree, size_t size);
 size_t binary_tree_leaves(const binary_tree_t *tree);
+size_t count_leaves(const binary_tree_t *tree, size_t leaves);
 
 #endif
